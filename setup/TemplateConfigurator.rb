aa9f0698c364a5c2778caa5111c6e5ff878a8632
wei.hu <7430328@qq.com>
2024-05-14 11:41:59 +0800

    feat(DEMO-0000): 默认支持iOS

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -70,7 +70,9 @@ module Pod
     def run
       @message_bank.welcome_message
 
-      platform = self.ask_with_answers("What platform do you want to use?", ["iOS", "macOS"]).to_sym
+      # platform = self.ask_with_answers("What platform do you want to use?", ["iOS", "macOS"]).to_sym
+      # 默认支持iOS
+      platform = :ios
 
       case platform
         when :macos
